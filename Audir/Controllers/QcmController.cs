fef204fd967fbce128462b0a05ad00b65545365b
Yazid <Yazid.Alaoui54@gmail.com>
2019-01-10 17:54:11 +0100

    ll

--- Audir/Controllers/QcmController.cs
+++ Audir/Controllers/QcmController.cs
@@ -1,17 +1,14 @@
 ﻿using Audir.Models;
+using CrystalDecisions.CrystalReports.Engine;
 using System.Collections.Generic;
-using System.Web.Mvc;
+using System.Data;
 using System.IO;
-
+using System.Web.Mvc;
 namespace Audir.Controllers
 {
     public class QcmController : Controller
     {
-        public List<Question> list_qst;
-        private object pdf;
-
-      
-       
+        public List<Question> list_qst;       
         public QcmController()
         {
             list_qst = new List<Question>
@@ -29,12 +26,34 @@ namespace Audir.Controllers
                 new Question(11, "est ce qu 'il a","Loguique"),
                 new Question(12, "est ce qu 'il a","Loguique"),
                 new Question(13, "est ce qu 'il a","Loguique")
+               
             };
         }
         public ActionResult Index()
         {
          
+
             return View(list_qst);
+        }
+        public ActionResult pdf()
+        {
+
+            foreach (var x in list_qst)
+            {
+                
+
+            }
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/"), "CrystalReport1.rpt"));
+
+            rd.SetDataSource(list_qst);
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            stream.Seek(0, SeekOrigin.Begin);
+            return File(stream, "application/pdf", "QuestionList.pdf");
         }
   
     }
