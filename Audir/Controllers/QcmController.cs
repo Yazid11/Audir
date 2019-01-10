b9fe997b65cc0320460a09dcf567509fae14f45a
Yazid <Yazid.Alaoui54@gmail.com>
2019-01-10 14:50:38 +0100

    k

---
+++ Audir/Controllers/QcmController.cs
@@ -0,0 +1,41 @@
+﻿using Audir.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Audir.Controllers
+{
+    public class QcmController : Controller
+    {
+        public List<Question> list_qst;
+        private object pdf;
+
+      
+       
+        public QcmController()
+        {
+            list_qst = new List<Question>
+            {
+                new Question(1, "est ce qu 'il a","physique"),
+                new Question(2, "est ce qu 'il a","physique"),
+                new Question(3, "est ce qu 'il a","physique"),
+                new Question(4, "est ce qu 'il a","physique"),
+                new Question(5, "est ce qu 'il a","physique"),
+                new Question(6, "est ce qu 'il a","physique"),
+                new Question(7, "est ce qu 'il a","physique"),
+                new Question(8, "est ce qu 'il a","Loguique"),
+                new Question(9, "est ce qu 'il a","Loguique"),
+                new Question(10, "est ce qu 'il a","Loguique"),
+                new Question(11, "est ce qu 'il a","Loguique"),
+                new Question(12, "est ce qu 'il a","Loguique"),
+                new Question(13, "est ce qu 'il a","Loguique")
+            };
+        }
+        public ActionResult Index()
+        {
+         
+            return View(list_qst);
+        }
+  
+    }
+}
---
+++ Audir/Models/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Audir.Models
+{
+    public class Question
+    {
+        private int id;
+        private string text;
+        private bool reponse;
+        private string type;
+        public Question()
+        {
+
+        }
+
+        public Question(int id, string text,string type)
+        {
+            this.id = id;
+            this.text = text;
+            this.Type = type;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Text { get => text; set => text = value; }
+        public bool Reponse { get => reponse; set => reponse = value; }
+        public string Type { get => type; set => type = value; }
+    }
+}
